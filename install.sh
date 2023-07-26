273bbe878beeab28e601ad16577e1e7ec0c92ca7
Jacob Kohn <jakekohn@me.com>
2023-07-25 21:05:58 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -4,24 +4,31 @@ CONFIG_DIR="$HOME/.config"
 NVIM_CONFIG="$(pwd)/nvim"
 
 cleanup() {
-  rm -rf "$CONFIG_DIR/nvim"
+  rm -rf "$CONFIG_DIR/nvim" 
   rm -rf "$HOME/.local/share/nvim/lazy"
   rm -rf "$HOME/.local/state/nvim/lazy"
 }
 
 while getopts 'd' opt; do
   case "$opt" in
-    d) cleanup
+    d) cleanup 2> /dev/null
        exit 0 ;;
     *) # do nothing
   esac
 done
 
+
 if [[ -d "$CONFIG_DIR/nvim" ]]
 then
 	echo "Config already exists, clean before continuing: $CONFIG_DIR/nvim"
-  echo "Usage: ./install.sh -d to clean to clean all dirs"
+  	echo "Usage: ./install.sh -d to clean to clean all dirs"
 	exit 1
 fi
 
-cp -r "$NVIM_CONFIG" "$CONFIG_DIR"
+if [[ $(find nvim -name "lazy.lua") ]]
+then
+	echo "Installing NVIM with LAZY package manager"
+	cp -r "$NVIM_CONFIG" "$CONFIG_DIR"
+  echo -e nvim --headless +Lazy Sync +qa
+	exit 0
+fi
