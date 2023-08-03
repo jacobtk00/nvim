766cc059bcd05ccd4b575027f8d6e10de2de1f3a
Jacob Kohn <jakekohn@me.com>
2023-08-02 21:54:20 -0400

    diagnostics or sumn

--- nvim/lua/plugins/lsp/diagnostics.lua
+++ nvim/lua/plugins/lsp/diagnostics.lua
@@ -9,14 +9,11 @@ function M.setup()
 		update_in_insert = false,
 		virtual_text = { spacing = 4, prefix = "●" },
 		severity_sort = true,
-		float = { border = "single" },
+		signs = true,
+		float = {
+			border = "single",
+		},
 	})
-
-	-- vim.lsp.handlers["workspace/diagnostic/refresh"] = function(_, _, ctx)
-	--   local ns = vim.lsp.diagnostic.get_namespace(ctx.client_id)
-	--   vim.diagnostic.reset(ns)
-	--   return vim.NIL
-	-- end
 
 	for type, icon in pairs(M.signs) do
 		local hl = "DiagnosticSign" .. type
@@ -25,3 +22,27 @@ function M.setup()
 end
 
 return M
+
+-- • {contents}  (table) of lines to show in window
+-- • {syntax}    (string) of syntax to set for opened buffer
+-- • {opts}      (table) with optional fields (additional keys are passed
+--               on to |nvim_open_win()|)
+--               • height: (integer) height of floating window
+--               • width: (integer) width of floating window
+--               • wrap: (boolean, default true) wrap long lines
+--               • wrap_at: (integer) character to wrap at for computing
+--                 height when wrap is enabled
+--               • max_width: (integer) maximal width of floating window
+--               • max_height: (integer) maximal height of floating window
+--               • pad_top: (integer) number of lines to pad contents at
+--                 top
+--               • pad_bottom: (integer) number of lines to pad contents at
+--                 bottom
+--               • focus_id: (string) if a popup with this id is opened,
+--                 then focus it
+--               • close_events: (table) list of events that closes the
+--                 floating window
+--               • focusable: (boolean, default true) Make float focusable
+--               • focus: (boolean, default true) If `true`, and if
+--                 {focusable} is also `true`, focus an existing floating
+--                 window with the same {focus_id}
