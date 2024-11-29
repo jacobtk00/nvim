02ab22c921a493b3851327397d1558dd5df3e2d4
Jacob <jakekohn@me.com>
2024-11-29 14:12:32 -0500

    latest

--- nvim/lua/plugins/lsp/on_attach.lua
+++ nvim/lua/plugins/lsp/on_attach.lua
@@ -1,60 +1,43 @@
 local on_attach = function(client, buffer)
 	-- local cap = client.server_capabilities
 	-- client.server_capabilities.semanticTokensProvider = nil
+	-- if client.name == "gopls" then
 	local keymaps = {
-		buffer = buffer,
-		["<leader>"] = {
-			r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
-			a = { name = "Code Action", c = { vim.lsp.buf.code_action, "Code Action" } },
-			c = {
-				name = "+code",
-				{
-					cond = client.name == "tsserver",
-					o = { "<cmd>TypescriptOrganizeImports<CR>", "Organize Imports" },
-					R = { "<cmd>TypescriptRenameFile<CR>", "Rename File" },
-				},
-				{
-					cond = client.name == "gopls",
-					m = {
-						name = "+Mod",
-						t = { "<cmd>GoModTidy<cr>", "Mod Tidy" },
-					},
-				},
-				d = { vim.diagnostic.open_float, "Line Diagnostics" },
-				l = {
-					name = "+lsp",
-					i = { "<cmd>LspInfo<cr>", "Lsp Info" },
-					a = { "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", "Add Folder" },
-					r = { "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove Folder" },
-					l = { "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-						"List Folders" },
-				},
-			},
-			x = { d = { "<cmd>Telescope diagnostics<cr>", "Search Diagnostics" } },
-		},
-		g = {
-			name = "+goto",
-			d = { "<cmd>Telescope lsp_definitions<cr>", "Goto Definition" },
-			r = { "<cmd>Telescope lsp_references<cr>", "References" },
-			R = { "<cmd>Trouble lsp_references<cr>", "Trouble References" },
-			D = { "<cmd>Telescope lsp_declarations<CR>", "Goto Declaration" },
-			I = { "<cmd>Telescope lsp_implementations<CR>", "Goto Implementation" },
-			t = { "<cmd>Telescope lsp_type_definitions<cr>", "Goto Type Definition" },
-			i = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Goto Implementation" },
-		},
-		["<C-k>"] = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help", mode = { "i" } },
-		["K"] = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
-		["[d"] = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "Next Diagnostic" },
-		["]d"] = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Prev Diagnostic" },
-		["[e"] = { "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})<CR>",
-			"Next Error" },
-		["]e"] = { "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})<CR>",
-			"Prev Error" },
-		["[w"] = { "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.WARNING})<CR>",
-			"Next Warning", },
-		["]w"] = { "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.WARNING})<CR>",
-			"Prev Warning", },
+		{ "<C-k>",       "<cmd>lua vim.lsp.buf.signature_help()<CR>",                                           desc = "Signature Help",      mode = "i" },
+		{ "<leader>a",   group = "Code Action" },
+		{ "<leader>ac",  vim.lsp.buf.code_action,                                                               desc = "Code Action" },
+		{ "<leader>c",   group = "code" },
+		{ "<leader>cd",  vim.diagnostic.open_float,                                                             desc = "Line Diagnostics" },
+		{ "<leader>cl",  group = "lsp" },
+		{ "<leader>cla", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>",                                     desc = "Add Folder" },
+		{ "<leader>cli", "<cmd>LspInfo<cr>",                                                                    desc = "Lsp Info" },
+		{ "<leader>cll", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",               desc = "List Folders" },
+		{ "<leader>clr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>",                                  desc = "Remove Folder" },
+		{ "<leader>r",   "<cmd>lua vim.lsp.buf.rename()<CR>",                                                   desc = "Rename" },
+		{ "<leader>xd",  "<cmd>Telescope diagnostics<cr>",                                                      desc = "Search Diagnostics" },
+		{ "K",           "<cmd>lua vim.lsp.buf.hover()<CR>",                                                    desc = "Hover" },
+		{ "[d",          "<cmd>lua vim.diagnostic.goto_prev()<CR>",                                             desc = "Next Diagnostic" },
+		{ "[e",          "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})<CR>",   desc = "Next Error" },
+		{ "[w",          "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.WARNING})<CR>", desc = "Next Warning" },
+		{ "]d",          "<cmd>lua vim.diagnostic.goto_next()<CR>",                                             desc = "Prev Diagnostic" },
+		{ "]e",          "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})<CR>",   desc = "Prev Error" },
+		{ "]w",          "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.WARNING})<CR>", desc = "Prev Warning" },
+		{ "g",           group = "goto" },
+		{ "gD",          "<cmd>Telescope lsp_declarations<CR>",                                                 desc = "Goto Declaration" },
+		{ "gI",          "<cmd>Telescope lsp_implementations<CR>",                                              desc = "Goto Implementation" },
+		{ "gR",          "<cmd>Trouble lsp_references<cr>",                                                     desc = "Trouble References" },
+		{ "gd",          "<cmd>Telescope lsp_definitions<cr>",                                                  desc = "Goto Definition" },
+		{ "gi",          "<cmd>lua vim.lsp.buf.implementation()<CR>",                                           desc = "Goto Implementation" },
+		{ "gr",          "<cmd>Telescope lsp_references<cr>",                                                   desc = "References" },
+		{ "gt",          "<cmd>Telescope lsp_type_definitions<cr>",                                             desc = "Goto Type Definition" },
 	}
+
+	local is_lsp_buf = function()
+		return vim.api.nvim_get_current_buf() == buffer
+	end
+	for _, keymap in ipairs(keymaps) do
+		keymap.cond = is_lsp_buf
+	end
 
 	-- Create a command `:Format` local to the LSP buffer
 	vim.api.nvim_buf_create_user_command(buffer, "Format", function(_)
@@ -62,7 +45,7 @@ local on_attach = function(client, buffer)
 	end, { desc = "Format current buffer with LSP" })
 
 
-	require("which-key").register(keymaps)
+	require("which-key").add(keymaps)
 end
 
 return on_attach
